c09206be1f39ca8bc8205f2e5e119222e5251be1
zhongxianjun <37361079+zhongxianjun@users.noreply.github.com>
2018-04-09 13:23:13 +0800

    4.9
    4.9

--- elm/src/js/resDetail.js
+++ elm/src/js/resDetail.js
@@ -5,3 +5,19 @@ import '../scss/resDetail.scss';
 import $ from 'jquery';
 
 import BScroll from 'better-scroll';
+
+
+$(function(){
+
+	// tab选项卡功能实现
+	$('.tab').delegate('a','click',function(){
+		$(this).addClass('on').siblings().removeClass('on');
+		var index = $(this).attr('index');
+		$('.tab-index').eq(index).addClass('on').siblings('div').removeClass('on');
+	});
+
+
+
+
+
+});
