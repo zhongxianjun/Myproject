ed3c47c5fb13656f79e84fd40c9b6841ffcf2f8d
zhongxianjun <37361079+zhongxianjun@users.noreply.github.com>
2018-04-18 13:27:02 +0800

    递归
    递归

---
+++ 递归/app/app.js
@@ -0,0 +1,16 @@
+//累加
+var vm = new Vue({
+	el: '#add',
+	data:{
+		inVal:0,
+		outVal:0
+	},
+	methods:{
+		cumulative:function(i){
+			if(i < 1){
+				return 0;
+			}
+			return this.outVal = parseInt(i) + this.cumulative(i-1);
+		}
+	}
+})
