f5b002d4e6b86f0d60a8c584682485f35fdf2270
zhongxianjun <37361079+zhongxianjun@users.noreply.github.com>
2018-04-23 19:27:28 +0800

    1
    1

---
+++ vue/vue 2.0/code/src/components/Son.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="">
+    {{title}}
+    <button @click="value">子向父传值</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Son',
+  data(){
+  	return {
+  		newValue:'猴塞给，托里阿克托'
+  	}
+  },
+  props:{
+    title:{
+      type:String,
+      default:'vue2.0使用'
+    }
+  },
+  methods:{
+    value:function(){
+      this.$emit('child-to-parent',this.newValue);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  button{
+    color:red;
+    }
+</style>
---
+++ vue/vue 2.0/code/src/pages/Indec.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="content">
+    <h1>{{title}}</h1>
+    <Son :title="title" @child-to-parent="changeVal"></Son>
+  </div>
+</template>
+
+<script>
+import Son from '@/components/Son'
+export default {
+  name: '',
+  data(){
+    return {
+      title:'vue2.0的使用'
+    }
+  },
+  methods:{
+    changeVal:function(val){
+      this.title += val;
+    }
+  },
+  components:{
+    Son
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
