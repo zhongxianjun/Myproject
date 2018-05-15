c18b0cc9ab58a483c03b1fdf646011989699eb4d
zhongxianjun <37361079+zhongxianjun@users.noreply.github.com>
2018-05-15 21:00:20 +0800

    1
    1

--- createjs/src/assets/js/Load.js
+++ createjs/src/assets/js/Load.js
@@ -2,4 +2,32 @@ import G from './gloable';
 
 export default function Load(){
 	this.progress = 0;
+	this._init();
+}
+
+var loadDom = null;
+var loadspanDom = null;
+Load.prototype._init = function(){
+	loadDom = document.querySelector('#progress');
+	loadspanDom = document.querySelector('#progress span');
+	this.queue = new createjs.LoadQueue();
+	//注册音频插件
+	this.queue.installPlugin(createjs.Sound);
+
+	//定义资源加载队列
+	this.queue.loadManifest([
+		{id:'homeBg', src:'../imges/homePage_684d8f7.jpg'},
+		{id:'musicBg', src:'../media/music.mp3'},
+
+		//场景一的资源
+		{id:'01_flower', src:'../images/flower_ddf789f.png'}
+	]);
+
+	//资源加载进度
+	this.queue.on('progress', handleProgress);
+	function handleProgress(e){
+		this.progress = e.progress;
+
+		loadspanDom.innerText = parseInt(this.progress * 100) + '%';
+	}
 }
